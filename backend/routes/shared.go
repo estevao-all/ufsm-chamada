cfa84f4df23b0fc571d6ca331ef20d8b599a4fce
Estevão <135400853+estevao-all@users.noreply.github.com>
2026-06-03 20:23:00 -0300

    Add disciplines parsing and listing to dashboard (#3)
    Co-authored-by: Nyss <straussnycolas@gmail.com>

--- backend/routes/shared.go
+++ backend/routes/shared.go
@@ -2,6 +2,12 @@ package routes
 
 var UFSM_PORTAL_HOST = "portal.ufsm.br"
 var UFSM_PORTAL_BASE_URL = "https://portal.ufsm.br"
-var UFSM_PORTAL_TYPE = "estudantil"
+var UFSM_PORTAL_TYPE = "docente"
+var UFSM_PORTAL_DWR_PLAINCALL_BASE_URL = UFSM_PORTAL_BASE_URL + "/" + UFSM_PORTAL_TYPE + "/dwr/call/plaincall"
+
 var UFSM_PORTAL_LOGIN_URL = UFSM_PORTAL_BASE_URL + "/" + UFSM_PORTAL_TYPE + "/j_security_check"
 var UFSM_PORTAL_INDEX_URL = UFSM_PORTAL_BASE_URL + "/" + UFSM_PORTAL_TYPE + "/index.html"
+var UFSM_PORTAL_MAIN_MENU_URL = UFSM_PORTAL_BASE_URL + "/" + UFSM_PORTAL_TYPE + "/mainMenu.html"
+
+var UFSM_PORTAL_SCHEDULE_URL = UFSM_PORTAL_DWR_PLAINCALL_BASE_URL + "/gradeHorariosAjaxService.horarios.dwr"
+var UFSM_PORTAL_CLASSES_URL = UFSM_PORTAL_BASE_URL + "/" + UFSM_PORTAL_TYPE + "/turma/turma.html?action=list"
